050203a5185f555bdb7dab33458b94e149650f0a
Mohammed Al-Khonaizi <backlife2005@gmail.com>
2017-02-24 17:19:17 +0300

    Added a swift fill for the second View Controller and named it GameViewController.
    Added an IBAction named cameraTapped.
    
    Added an IBAction named photosTapped.

---
+++ GameCollector/GameViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by Mohammed Al-Khonaizi on 2/24/17.
+//  Copyright © 2017 Mohammed Al-Khonaizi. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+}
